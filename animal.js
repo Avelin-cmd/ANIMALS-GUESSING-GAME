6b49c35bdbbbe9bed808645c56a31b3297c93753
Avelin-cmd <avelinlohay@gmail.com>
2026-06-29 18:11:43 +0300

    Html amd Js

---
+++ animal.js
@@ -0,0 +1,22 @@
+let animal = prompt ("enter the animal")
+for(let i = 1; i <=5; i++){
+    if(animal == "hen"){
+        alert("kukoliko");
+    }
+    else if(animal == "dog"){
+        alert("wuwuwu");
+    }
+    else if(animal == "cat"){
+        alert("mwau mwau");
+    }
+    else if(animal == "duck"){
+        alert("kwa kwa");
+    }
+    else if(animal == "rabit"){
+        alert("chwi chiw");
+    }
+    else{
+        alert("jaributena");
+    }
+}
+alert("the game is over")
